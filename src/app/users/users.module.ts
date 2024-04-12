85713d8f9cf9d9f31abe404a6e5415da27e45655
SimoneFilograna <simonefilogranaf12@gmail.com>
2024-04-12 17:39:52 +0200

    fix-module-app

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HidePassPipe } from '../core/pipes/hide-pass.pipe';
-import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    HidePassPipe,
-    EditUserComponent,
+    HidePassPipe
   ],
   imports: [
     CommonModule,
